7b3545d8ccadf26fe36fee3e2bf73468bee83573
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2022-05-14 21:28:04 +0530

    url_code

--- urls.py
+++ urls.py
@@ -19,3 +19,15 @@ urlpatterns = [
       path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
 ===================================================================================================================================================
+from django.urls import path
+from . import views
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+=======================================================================================================================================================
+<li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
+<li><a href="{% url 'polls:detail' question.id %}">{{ question.question_text }}</a></li>
